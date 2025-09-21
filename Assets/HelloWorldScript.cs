09c6c907d429004520c198f9c983d7be76c1dbd4
Davidgregory7 <dgregorykg349@gmail.com>
2025-09-21 12:33:33 -0700

    Initial commit: Added Unity Helo World project with .gitignore and .gitattributes

---
+++ Assets/HelloWorldScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class HelloWorldScript : MonoBehaviour
+{
+    // Called once when the GameObject is loaded into the scene
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+}
