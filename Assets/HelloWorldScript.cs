fbd5ba4083bf477cf447fbedc713db5a2e3265af
Davidgregory7 <dgregorykg349@gmail.com>
2025-09-25 12:12:08 -0700

    Dev: Update HelloWorldScript to print branch-specific message

--- Assets/HelloWorldScript.cs
+++ Assets/HelloWorldScript.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 
+[System.Diagnostics.DebuggerDisplay("{" + nameof(GetDebuggerDisplay) + "(),nq}")]
 public class HelloWorldScript : MonoBehaviour
 {
     // Called once when the GameObject is loaded into the scene
     void Start()
     {
-        Debug.Log("Hello World");
+        Debug.Log("Hello World from Development Branch!");
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
     }
 }
